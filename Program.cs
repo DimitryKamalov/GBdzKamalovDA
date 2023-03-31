4cf2d0ee6714b2fe143199f8d9795e3cd6fd1f69
kda <kom-d@yandex.ru>
2023-03-31 15:56:29 +0300

    Creating the new massiv

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        // объявление размера массива
+        Console.Write("Введите количество элементов массива: ");
+        int n = int.Parse(Console.ReadLine());
+        string[] arr = new string[n];
+
+        
+}
